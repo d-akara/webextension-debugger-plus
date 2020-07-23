1ce16eadecfc026ca3f4a15fb0c917ada7b6549a
Dakara <d-akara@users.noreply.github.com>
2020-07-22 20:42:06 -0400

    updates

--- src/webextension/options/Options.ts
+++ src/webextension/options/Options.ts
@@ -4,7 +4,7 @@ const log = wx.makeLogger('Options')
 log.log('Options loaded')
 
 try {
-    wx.storage.memGet('count').then(value => {
+    wx.storage.memGet('test').then(value => {
         log.log(value);
         const newImageHTMLElement = document.createElement("div");
         newImageHTMLElement.innerText = value as Object as string
@@ -13,14 +13,9 @@ try {
         log.log(JSON.stringify(reason))
     });
 
-    wx.sendMessageTabs({}, { event: "popup.event", content: 'message from options' }).then(response => {
-        response.filter( r => !r.isError ).forEach( r => log.log('promise all response', wx.tabInfo(r.tab), r.content))
-    }).catch(reason => {
-        log.log('handle error', JSON.stringify(reason))
-    });
 
-    wx.sendMessageExtensionPages({ event: "webextension.ping", content: 'ping from devtools' }).then(response => log.log(response));
-    wx.subscribeMessages('webextension.ping', event => 'ping response Options')
+    wx.sendMessageExtensionPages({ event: "webextension.ping", content: 'ping from options' }).then(response => log.log(response));
+    wx.subscribeMessages('test.ping', event => log.log(event.content))
 } catch (error) {
     console.log(error)
     log.log(JSON.stringify(error))
